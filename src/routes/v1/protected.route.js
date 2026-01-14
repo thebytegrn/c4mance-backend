f2191de0b35e341ca40af96d4c22b00045a66c50
thebytegrn <thebrightsmartgreen@gmail.com>
2026-01-14 08:44:58 +0100

    chore: add logo URL after upload to organization

--- src/routes/v1/protected.route.js
+++ src/routes/v1/protected.route.js
@@ -3,6 +3,7 @@ import { authMiddleware } from "../../middlewares/auth.middleware.js";
 import { createOrgService } from "../../services/createOrg.service.js";
 import { upload } from "../../utils/upload.util.js";
 import mongoose from "mongoose";
+import { Organization } from "../../models/organization.model.js";
 
 const protectedRouter = Router();
 
@@ -31,8 +32,25 @@ protectedRouter.post(
     next();
   },
   upload.single("logoFile"),
-  (req, res) => {
-    res.status(200).json({ success: true, message: "Upload successful" });
+  async (req, res) => {
+    try {
+      if (!req.file)
+        return res
+          .status(400)
+          .json({ success: false, message: "File not selected" });
+
+      const bucket = "https://c4mance.com";
+      await Organization.findByIdAndUpdate(req.authUser.organizationId, {
+        $set: { logoURL: bucket + "/" + req.file.key },
+      });
+
+      return res
+        .status(200)
+        .json({ success: true, message: "Logo upload successful" });
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
   }
 );
 
