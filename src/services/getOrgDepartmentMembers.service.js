22cc258e67509b1f4404d35dd0b2a3c3d9c2c0be
thebytegrn <thebrightsmartgreen@gmail.com>
2026-01-24 13:25:11 +0100

    refactor: paginate get department members

--- src/services/getOrgDepartmentMembers.service.js
+++ src/services/getOrgDepartmentMembers.service.js
@@ -3,28 +3,39 @@ import { User } from "../models/user.model.js";
 
 export const getOrgDepartmentMembers = async (req, res) => {
   try {
+    const { cursor } = req.query;
+    const limit = 10;
     const departmentId = req.params?.departmentId;
 
-    if (!mongoose.isValidObjectId(departmentId))
+    if (!mongoose.isValidObjectId(departmentId)) {
       return res
         .status(400)
         .json({ success: false, message: "Invalid department ID" });
+    }
 
-    const members = await User.find({ departmentId })
+    const filter = { departmentId };
+    if (mongoose.isValidObjectId(cursor)) {
+      filter._id = { $gt: cursor };
+    }
+
+    const members = await User.find(filter)
+      .sort({ _id: 1 })
+      .limit(limit)
       .select("-password")
-      .exec();
+      .lean();
 
-    if (!members.length)
-      res.status(200).json({
-        success: true,
-        messsage: "Department members",
-        data: { totalCount: 0, members },
-      });
+    let nextCursor = null;
+    if (members.length === limit) {
+      nextCursor = members[members.length - 1]._id;
+    }
 
-    res.status(200).json({
+    return res.status(200).json({
       success: true,
-      messsage: "Department members",
-      data: { totalCount: members.length, members },
+      message: "Department members",
+      data: {
+        members,
+        nextCursor,
+      },
     });
   } catch (error) {
     console.log(error);
