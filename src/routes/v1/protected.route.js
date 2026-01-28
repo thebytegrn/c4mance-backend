5db3ca83d699e512b44b4fd66cc1c9935e22b30d
thebytegrn <thebrightsmartgreen@gmail.com>
2026-01-28 21:32:53 +0100

    chore: decouple upload logo service

--- src/routes/v1/protected.route.js
+++ src/routes/v1/protected.route.js
@@ -2,7 +2,6 @@ import { Router } from "express";
 import { authMiddleware } from "../../middlewares/auth.middleware.js";
 import { createOrgService } from "../../services/createOrg.service.js";
 import { upload } from "../../utils/upload.util.js";
-import { Organization } from "../../models/organization.model.js";
 
 import { isAdminUser } from "../../middlewares/isAdminUser.middleware.js";
 import { isRootUser } from "../../middlewares/isRootUser.middleware.js";
@@ -23,6 +22,8 @@ import { disableOrgDepartment } from "../../services/disableOrgDepartment.servic
 import { deleteOrgDepartment } from "../../services/deleteOrgDepartment.service.js";
 import { editOrgMemberProfile } from "../../services/editOrgMemberProfile.service.js";
 import { changeOrgMemberPassword } from "../../services/changeOrgMemberPassword.service.js";
+import { getMemberProfile } from "../../services/getMemberProfile.service.js";
+import { uploadLogo } from "../../services/uploadOrgLogo.service.js";
 
 const protectedRouter = Router();
 
@@ -30,8 +31,9 @@ protectedRouter.use(authMiddleware);
 
 protectedRouter.post("/orgs", isRootUser, createOrgService);
 protectedRouter.get("/orgs/members/search", searchOrgEmployees);
-protectedRouter.post("/orgs/member/invite", inviteMemberService);
+protectedRouter.post("/orgs/members/invite", inviteMemberService);
 protectedRouter.get("/orgs/members", getPaginatedOrgMembers);
+protectedRouter.get("/orgs/members/profile", getMemberProfile);
 protectedRouter.get(
   "/orgs/reportlineup/:assignedDepartmentalRole",
   getReportLineUpService,
@@ -66,26 +68,7 @@ protectedRouter.post(
   hasOrganization,
   isAdminUser,
   upload.single("logoFile"),
-  async (req, res) => {
-    try {
-      if (!req.file)
-        return res
-          .status(400)
-          .json({ success: false, message: "File not selected" });
-
-      const bucket = "https://static.c4mance.com";
-      await Organization.findByIdAndUpdate(req.authUser.organizationId, {
-        $set: { logoURL: bucket + "/" + req.file.key },
-      }).exec();
-
-      return res
-        .status(200)
-        .json({ success: true, message: "Logo upload successful" });
-    } catch (error) {
-      console.log(error);
-      throw error;
-    }
-  },
+  uploadLogo,
 );
 
 export default protectedRouter;
