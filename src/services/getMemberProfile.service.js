ebd86bfea7581fc9118dfed1b74239bb41a275a9
thebytegrn <thebrightsmartgreen@gmail.com>
2026-01-29 09:21:29 +0100

    feat: add get member profile

--- src/services/getMemberProfile.service.js
+++ src/services/getMemberProfile.service.js
@@ -1,6 +1,17 @@
+import { User } from "../models/user.model.js";
+
 export const getMemberProfile = async (req, res) => {
   try {
-    res.send("ok");
+    const user = req.authUser._id;
+
+    const profile = await User.findById(user)
+      .populate("departmentId")
+      .select("-password")
+      .exec();
+
+    return res
+      .status(200)
+      .json({ success: true, message: "Member Profile", data: { profile } });
   } catch (error) {
     console.log("Error getting member profile", error);
     throw error;
