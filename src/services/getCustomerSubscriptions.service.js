bf7e739130596881082580004bad7d2a948e8b44
thebytegrn <thebrightsmartgreen@gmail.com>
2026-02-04 17:02:26 +0100

    feat: get default sub plan

--- src/services/getCustomerSubscriptions.service.js
+++ src/services/getCustomerSubscriptions.service.js
@@ -1,12 +1,29 @@
+import { BillingPlan } from "../models/billingPlan.model.js";
 import { Subscription } from "../models/subscription.model.js";
 
 export const getCustomerSubscriptions = async (req, res) => {
   try {
-    const subs = await Subscription.findOne({
+    const customerSub = await Subscription.findOne({
       customer: req.authUser._id,
     }).exec();
-    console.log(subs);
-    res.send("subs plans");
+
+    const subscriptionPlans = await BillingPlan.find().lean().exec();
+
+    const customerSubPlansWithDefault = subscriptionPlans.map((plan) => {
+      if (plan.planCode === customerSub.plan) {
+        plan.default = true;
+      }
+
+      return plan;
+    });
+
+    return res
+      .status(200)
+      .json({
+        success: true,
+        message: "Default plan",
+        data: { plans: customerSubPlansWithDefault },
+      });
   } catch (error) {
     console.log("Error getting cutomer subscriptions", error);
     throw error;
